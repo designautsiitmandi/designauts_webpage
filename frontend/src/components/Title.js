383247e40b8f1e6d5fa7cb0ef709bcbf1659e9cb
RishabhGarg108 <rishabhgarg108@gmail.com>
2020-12-03 19:18:16 +0530

    Initialized Project

---
+++ frontend/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>Designauts, IIT Mandi</h1>
+      <h3>The Designing Club of IIT, Mandi</h3>
+    </div>
+  );
+}
+
+export default Title;
